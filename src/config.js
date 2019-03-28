7f0503dc31f39a961e57f38bc9189f0254743921
yohanesam <yohanesam@users.noreply.github.com>
2019-03-29 06:25:42 +0700

    apis config

---
+++ src/config.js
@@ -0,0 +1,24 @@
+// Configuration for The Movie DB
+
+const API_URL = 'http://api.themoviedb.org/3/';
+const API_KEY = '15b33fe7ac812b60a48790d69fc5ec71';
+
+// Image
+// An image URL looks like this example :
+// http://image.tmdb.org/t/p/w780/yourimage.jpg
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+
+// Sizes : w300, w780, w1280, original
+const BACKDROP_SIZE = 'w1280';
+
+// w92, w154, w185, w342, w500, w700, original
+const POSTER_SIZE = 'w500'
+
+export {
+    API_URL,
+    API_KEY,
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE
+}
