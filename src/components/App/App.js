50de15567c49a18466df43f98783c075b92fbc81
yohanesam <yohanesam@users.noreply.github.com>
2019-04-03 07:27:33 +0700

    route to movie page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Headers from '../elements/Header/Header'
 import Home from '../Home/Home'
+import Movie from '../Movie/Movie'
+import NotFound from '../elements/NotFound/NotFound'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Headers />
-        <Home />
-    </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <Headers />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/:movieId" component={Movie} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return(
+        <div>
+            <h1>Whoooopps!!! What you seek is doesn't exist</h1>
+        </div>
+    )
+
+}
+
+export default NotFound;
