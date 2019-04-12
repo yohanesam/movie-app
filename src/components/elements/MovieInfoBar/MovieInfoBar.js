9ee9d76cb01f7ca5d1e50aae7a38f65302a3b3d5
yohanesam <yohanesam@users.noreply.github.com>
2019-04-12 11:19:48 +0700

    add movie description

---
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { calcTime, convertMoney } from '../../../helper';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faMoneyBill, faTicketAlt } from '@fortawesome/free-solid-svg-icons'
+import './MovieInfoBar.css';
+const MovieInfoBar = (props) => {
+    return(
+        <div className="rmdb-movieinfobar">
+            <div className="rmdb-movieinfobar-content">
+                <div className="rmdb-movieinfobar-content-col">
+                    <FontAwesomeIcon icon={ faClock } className="fa-time" name="clock-o" size="2x" />
+                    <span className="rmdb-movieinfobar-info">Running Time :{ calcTime(props.time) }</span>
+                </div>
+
+                <div className="rmdb-movieinfobar-content-col">
+                    <FontAwesomeIcon icon={ faMoneyBill } className="fa-budget" name="clock-o" size="2x" />
+                    <span className="rmdb-movieinfobar-info">Budget: { convertMoney(props.budget) }</span>
+                </div>
+
+                <div className="rmdb-movieinfobar-content-col">
+                    <FontAwesomeIcon icon={ faTicketAlt } className="fa-revenue" name="ticket" size="2x" />
+                    <span className="rmdb-movieinfobar-info">Revenue: { convertMoney(props.revenue) }</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieInfoBar;
